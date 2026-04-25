3eae562ccb530231876587b77af170aecd630eb4
Anton Kostov <tonigreat@abv.bg>
2026-04-26 00:46:25 +0300

    updated alert repository for fetching and resolving alerts

--- backend/InvenTU.Core/Contracts/Repositories/IAlertRepository.cs
+++ backend/InvenTU.Core/Contracts/Repositories/IAlertRepository.cs
@@ -1,4 +1,5 @@
 using InvenTU.Core.Entities;
+using InvenTU.Core.Enums;
 
 namespace InvenTU.Core.Contracts.Repositories;
 
@@ -6,4 +7,6 @@ public interface IAlertRepository
 {
     Task<Guid> CreateAsync(Alert alert, CancellationToken cancellationToken = default);
     Task CreateUserStatesAsync(Guid alertId, IEnumerable<Guid> userIds, CancellationToken cancellationToken = default);
+    Task<Alert?> UnresolvedAlertForProductAsync(Guid productId, AlertType alertType, CancellationToken cancellationToken = default);
+    Task ResolveAsync(Guid alertId, CancellationToken cancellationToken = default);
 }
