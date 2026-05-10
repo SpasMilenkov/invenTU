f764d2e701185bdc0697f8d13bdb64eb622193d4
Anton Kostov <tonigreat@abv.bg>
2026-05-10 16:16:46 +0300

    connect to testdb for int testing

--- InvenTU.Tests/Integration/InvenTUApplicationFactory.cs
+++ InvenTU.Tests/Integration/InvenTUApplicationFactory.cs
@@ -1,7 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using InvenTU.Infrastructure.Data;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using InvenTU.Infrastructure.DataSeeders;
+using Microsoft.Extensions.Configuration;
 
 namespace InvenTU.Tests.Integration;
 
 public sealed class InvenTUApplicationFactory : WebApplicationFactory<Program>
 {
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        //todo: init Integration settings
+        builder.UseEnvironment("Development");
+
+        builder.ConfigureServices(services =>
+        {
+            // Inject config
+            var provider = services.BuildServiceProvider();
+            using var scope = provider.CreateScope();
+            var config = scope.ServiceProvider.GetRequiredService<IConfiguration>();
+
+            // Remove actual DB Context
+            var realDbContextdescriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(DbContextOptions<InvenTUDbContext>));
+            if (realDbContextdescriptor != null)
+                services.Remove(realDbContextdescriptor);
+
+            // Connect to dedicated testing DB
+            services.AddDbContext<InvenTUDbContext>(options =>
+                options.UseNpgsql(config.GetConnectionString("IntegrationTestConnection")));
+        });
+    }
 }
