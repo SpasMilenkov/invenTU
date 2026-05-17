7cd37f1471d05ec6d5ff587d8110ba5aa5117626
Stefan Gavrailov <stefan.gavrailov01@gmail.com>
2026-05-17 19:19:21 +0300

    Update reorder point validation to ensure it is greater than or equal to MinStockLevel

--- backend/InvenTU.Application/Products/Validators/CreateProductRequestValidator.cs
+++ backend/InvenTU.Application/Products/Validators/CreateProductRequestValidator.cs
@@ -40,6 +40,6 @@ public sealed class CreateProductRequestValidator : AbstractValidator<CreateProd
 
         RuleFor(x => x.ReorderPoint)
             .GreaterThanOrEqualTo(0).WithMessage("ReorderPoint must be 0 or greater.")
-            .LessThanOrEqualTo(x => x.MinStockLevel).WithMessage("ReorderPoint must be less than or equal to MaxStockLevel.");
+            .GreaterThanOrEqualTo(x => x.MinStockLevel).WithMessage("ReorderPoint must be greater than or equal to MinStockLevel.");
     }
 }
